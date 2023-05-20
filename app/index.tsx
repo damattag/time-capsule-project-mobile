7f90e699b3e56079a3ed8309db699d1797a7fd35
damattag <guilherme.damatta@yahoo.com.br>
2023-05-20 16:42:58 -0300

    finished new memory screen

--- app/index.tsx
+++ app/index.tsx
@@ -19,7 +19,7 @@ export default function App() {
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
-      clientId: "d26f194cc5d5132a51be",
+      clientId: "dc1e22655bb462ea2a46",
       scopes: ["identity"],
       redirectUri: makeRedirectUri({
         scheme: "nlwspacetime",
@@ -41,12 +41,12 @@ export default function App() {
   }
 
   useEffect(() => {
-    // console.log(
-    //   'response',
-    //   makeRedirectUri({
-    //     scheme: 'nlwspacetime',
-    //   }),
-    // )
+    /* console.log(
+      "response",
+      makeRedirectUri({
+        scheme: "nlwspacetime",
+      })
+    ); */
 
     if (response?.type === "success") {
       const { code } = response.params;
